8a78175f5350064d5f9749fcc5cd2294c8a92120
sergeymamonov <sergei.mamonov@gmail.com>
2020-07-01 19:18:25 +0300

    Authorization added

--- foodsharing.py
+++ foodsharing.py
@@ -1,31 +1,42 @@
-# import vk
+import vk_api
 import requests
 import json
 
-# ACCESS_TOKEN = 'a2ad71b9a36ae1748f12a9b3bff7b580f415d8ea3504c95c9fc70c5d28c49bc2fb6d097fbf20b167f3111'
-# VERSION = '5.110'
+
+def authorization():
+	login, password = '+7____', '____'
+	vk_session = vk_api.VkApi(login, password)
+	try:
+		vk_session.auth(token_only = False)
+	except vk_api.AuthError as error_msg:
+		print(error_msg)
+	vk = vk_session.get_api()
+	return vk
+
+def getinformation(vk):
+	foodsharingroup = '-109125816'
+	gruop = vk.wall.get(owner_id=foodsharingroup, count=2)
+	write_json(gruop)
+	# tools = vk_api.VkTools(vk_session)
+	# DOMAIN = 'sharingfood'
+	# wall = tools.get_all('wall.get', 5, {'domain': DOMAIN})
+	# write_json(wall.json())
+
 
 
 
 def write_json(data):
-	with open('post.json','w') as file:
-		json.dump(data, file, indent=2, ensure_ascii=False)
+	with open('information_from_group.json','w') as file:
+		json.dump(data, file, indent=2)#, ensure_ascii=False)
 
 
 def main():
-	DOMAIN = 'sharingfood'
-	response = requests.get('https://api.vk.com/method/wall.get', params={'domain': DOMAIN})
-	write_json(response.json())
+	vk = authorization()
+	getinformation(vk)
+	# response = requests.get('https://api.vk.com/method/wall.get', params={'domain': DOMAIN})
+	# write_json(response.json())
 
 
 
 if __name__ == '__main__':
 	main()
-
-
-# ?&access_token=ACCESS_TOKEN&v=VERSION
-
-# session = vk.Session(access_token='tocken')
-
-# gruop = vk.wall.get(owner_id=newstr, count=depth)
-# u = JSON.dumps(gruop)
